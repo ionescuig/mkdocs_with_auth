99423dc020665df48905f0c2eacf147de51169b5
Gabriel <ionescuig@yahoo.com>
2021-06-18 14:23:45 +0100

    Add static_root

--- mkdocs_with_auth/settings/production.py
+++ mkdocs_with_auth/settings/production.py
@@ -10,3 +10,5 @@ import dj_database_url
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
 DATABASES['default']['CONN_MAX_AGE'] = 500
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
