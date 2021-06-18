91036e0874c26dfd2536aae93200b4208a2b4ea4
Gabriel <ionescuig@yahoo.com>
2021-06-18 13:19:07 +0100

    Remove comments

--- mkdocs_with_auth/settings/production.py
+++ mkdocs_with_auth/settings/production.py
@@ -6,13 +6,11 @@ DEBUG = False
 ALLOWED_HOSTS = ['mkdocs-with-auth.herokuapp.com']        # you can add '<custom-domain>.com' in the list
 
 # ---=== DATABASE ===---
-# after DATABASES = {...} add this:
 import dj_database_url
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
 DATABASES['default']['CONN_MAX_AGE'] = 500
 
 # ---=== STATICFILES ===---
-# at the end of the file
 STATIC_URL = 'staticfiles_dir'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
